cded5b7ff4ed3ede6940b7fd56b679a29a749ec1
Paolo Baesso <paolo.baesso@rymapt.com>
2017-07-05 14:14:38 +0000

    Continue class definition

--- scripts/ISL12026.py
+++ scripts/ISL12026.py
@@ -10,6 +10,10 @@ class ISL12026:
 		#bus = smbus.SMBus(1)
 		self.bus= bus
 		self.DEVICE_ADDRESS = 0x6F      #7 bit address (will be left shifted to add the read write bit)
+		self.DATE_LIST= ['SEC', 'MIN', 'HR', 'DATE', 'MONTH', 'YR', 'DAY', 'Y2K']
+	
+	def __parseDate(self, datetimeList):
+		print datetimeList
 			
 	def get_bit(byteval,idx):
 		return ((byteval&(1<<idx))!=0);
@@ -31,3 +35,12 @@ class ISL12026:
 	def getStatus(self):
 		statusReg= self.getReg(0x3F)
 		return statusReg
+		
+	def getDate(self):
+		myDateTime=[]
+		for iData in range(0,8):
+			self.bus.write_byte_data(self.DEVICE_ADDRESS, 0x00, 0x30+iData) # the 0x00, 0x00 is the '16 bit' address split into 2 bytes
+			myData = self.bus.read_byte(self.DEVICE_ADDRESS) # this will read at the current address pointer, which we on the previous line
+			myDateTime.append(myData)
+			print self.DATE_LIST[iData], hex(myData)
+		self.__parseDate(myDateTime)
--- scripts/RTC_test.py
+++ scripts/RTC_test.py
@@ -6,3 +6,4 @@ bus = smbus.SMBus(1)
 myRTC= ISL12026(bus)
 
 print hex(myRTC.getStatus())
+myRTC.getDate()
