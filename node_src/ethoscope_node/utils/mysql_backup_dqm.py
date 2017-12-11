17ddff4ac648deb4b9acdc6b9484bffd9c071f32
Ryan Page <ryan.page@bristol.ac.uk>
2017-12-11 09:46:17 +0000

    Added async lock to stop and start thread for db backup

--- node_src/ethoscope_node/utils/mysql_backup_dqm.py
+++ node_src/ethoscope_node/utils/mysql_backup_dqm.py
@@ -17,6 +17,7 @@ class MySQLdbBackupRunner(object):
         self.ptime = ptime
         self.table_setup = False
         self.dbname_backup = None
+        self.condition = None
         self.running = False
         if dbconv != None:
             self.dbconv = dbconv
@@ -45,16 +46,24 @@ class MySQLdbBackupRunner(object):
         """
         self.dbconv.update_database()
 
-    def updatedb(self):
+    def _updatedb(self, condition):
         """
         Carry out the update after waiting for ptime minutes. If this is the first time it is run
         it will copy the tables and then after just append the data
         """
-        time.sleep(self.ptime*60)
-        if self.table_setup == False:
-            self.dbconv.copy_database("sqlite:////"+self.dbname_backup, skip_tables=self.skip_tables)
-            self.table_setup = True
-        self.dbconv.update_database()
+        condition.acquire()
+        while(True):
+
+            condition.wait(float(self.ptime)*60.0)
+
+            if self.running == False:
+                break
+            if self.table_setup == False:
+                self.dbconv.copy_database("sqlite:////"+self.dbname_backup, skip_tables=self.skip_tables)
+                self.table_setup = True
+            else:
+                self.dbconv.update_database()
+        condition.release()
 
     def runbackup(self, resume_run=False):
         """
@@ -68,13 +77,17 @@ class MySQLdbBackupRunner(object):
                 raise Exception("Error no backup file name has been specified")
             if os.path.isfile(self.dbname_backup):
                 os.remove(self.dbname_backup)
-            self.db_thread = threading.Thread(target=self.updatedb)
+            self.condition = threading.Condition()
+            self.db_thread = threading.Thread(target=self._updatedb, args=(self.condition, ))
             self.running = True
+
             self.db_thread.start()
         else:
-            self.db_thread = threading.Thread(target=self.updatedb)
+            self.condition = threading.Condition()
+            self.db_thread = threading.Thread(target=self.updatedb, args=(self.condition, ))
             self.running = True
             self.db_thread.start()
+
 
 
     def stopbackup(self):
@@ -82,4 +95,7 @@ class MySQLdbBackupRunner(object):
         Stop thread - TODO check that this will join once the function has finished executing
         """
         self.running = False
-        self.db_thread.join()
+        self.condition.acquire()
+        self.condition.notify()
+        self.condition.release()
+        
